5eb8705ed48c1802034cbf23cecd44760621b9f2
Luisa Scavo <luisascavo@gmail.com>
2017-09-28 15:34:00 -0400

    make prop name generic

--- client/src/components/MenuOption.js
+++ client/src/components/MenuOption.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class MenuOption extends Component {
   render(){
     return(
-      <option value={this.props.tool}>{this.props.text}</option>
+      <option value={this.props.value}>{this.props.text}</option>
     )
   }
 }
