3c4a59f44c6032ee44bc5bb41fdd224ad62e9263
Luisa Scavo <luisascavo@gmail.com>
2017-09-28 15:01:48 -0400

    stub out component

---
+++ client/src/components/ToolSelect.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class ToolSelect extends Component {
+  render(){
+    return(
+      <select>
+      </select>
+    )
+  }
+}
+
+export default ToolSelect;
