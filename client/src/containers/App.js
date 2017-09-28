11f50bb4a1721cb475acab937156d11318541848
Luisa Scavo <luisascavo@gmail.com>
2017-09-28 13:17:26 -0400

    import react-router methods, remove placeholder text

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
 import '../App.css';
-import Canvas from './containers/Canvas'
+import Canvas from './Canvas'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Canvas/>
+      <div>
+        <Router>
+          <Route exact path='/' component={Canvas}/>
+        </Router>
       </div>
     );
   }
