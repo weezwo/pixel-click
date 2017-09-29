6cb8749c7f1e5c7ee944a13d1c3e925259d7a33c
Luisa Scavo <luisascavo@gmail.com>
2017-09-29 14:07:35 -0400

    build out controller methods and sketch_params

--- app/controllers/sketch_controller.rb
+++ app/controllers/sketch_controller.rb
@@ -1,10 +1,21 @@
 class SketchController < ApplicationController
   def index
+    @sketches = Sketch.all
+    render json: @sketches
   end
 
   def show
+    @sketch = Sketch.find(params[:id])
+    render json: @sketch
   end
 
   def create
+    @sketch = Sketch.create(sketch_params)
+    render json: @sketch, status: 201
+  end
+
+  private
+  def sketch_params
+    params.permit(data: [])
   end
 end
