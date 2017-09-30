3247ac5e33783d48cd1364672fc940738c4f4368
Luisa Scavo <luisascavo@gmail.com>
2017-09-30 17:41:52 -0400

    map state to props, map dispatch to props, export connected component

--- client/src/containers/SketchesPage.js
+++ client/src/containers/SketchesPage.js
@@ -1,9 +1,23 @@
 import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import { bindActionCreators } from 'redux'
+import * as actions from '../actions/sketchActions'
 
-export default class SketchesPage extends Component {
+
+class SketchesPage extends Component {
   render(){
     return(
       <div>HEY</div>
     )
   }
 }
+
+function mapStateToProps(state) {
+  return {sketches: state.sketches.sketches}
+}
+
+function mapDispatchToProps(dispatch) {
+  return {actions: bindActionCreators(actions, dispatch)}
+}
+
+export default SketchesPage = connect(mapStateToProps, mapDispatchToProps)(SketchesPage)
