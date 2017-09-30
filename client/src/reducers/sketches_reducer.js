97a024b5e387bfa356918f125c8c1d68c1f7d7bf
Luisa Scavo <luisascavo@gmail.com>
2017-09-30 09:28:58 -0400

    remove unnecessary ADD_SKETCH action type

--- client/src/reducers/sketches_reducer.js
+++ client/src/reducers/sketches_reducer.js
@@ -2,9 +2,6 @@ export default (state = {loading: false, sketches: []}, action) => {
   switch (action.type) {
     case 'LOADING_SKETCHES':
       return Object.assign({},state,{loading: true})
-    case 'ADD_SKETCH':
-      const sketch = Object.assign({}, action.sketch)
-      return {loading: false, sketch: sketch}
     case 'FETCH_SKETCH':
       return {loading: false, sketch: action.payload}
     case 'FETCH_SKETCHES':
