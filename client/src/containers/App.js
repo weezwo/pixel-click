9c27dfa6b6a4b70fb872c6244987a9e51eabbc65
Luisa Scavo <luisascavo@gmail.com>
2017-09-30 12:46:56 -0400

    add route to sketches index, add Switch to handle routing

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
+  Switch,
   Route
 } from 'react-router-dom'
 import '../App.css';
 import Canvas from './Canvas'
+import SketchesPage from './SketchesPage'
 
 class App extends Component {
   render() {
     return (
       <div>
       <Router>
-        <Route exact path='/' component={Canvas}/>
+        <Switch>
+          <Route exact path='/' component={Canvas}/>
+          <Route path='/sketches' component={SketchesPage}/>
+        </Switch>
       </Router>
       </div>
     );
