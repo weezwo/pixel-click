d88765244c89c73d9d704c41d6f4ac0c07b6c40d
Luisa Scavo <luisascavo@gmail.com>
2017-10-01 17:37:42 -0400

    attempt to make functional with change from sketch to sketches

--- client/src/reducers/sketches_reducer.js
+++ client/src/reducers/sketches_reducer.js
@@ -3,7 +3,7 @@ export default (state = {loading: false, sketches: []}, action) => {
     case 'LOADING_SKETCHES':
       return Object.assign({},state,{loading: true})
     case 'FETCH_SKETCH':
-      return {loading: false, sketch: action.payload}
+      return {loading: false, sketches: action.payload}
     case 'FETCH_SKETCHES':
       return {loading: false, sketches: action.payload}
     default:
