9207c9dd27bea89ff0b52f29a7cb554a1f5d2ba5
Luisa Scavo <luisascavo@gmail.com>
2017-10-01 12:32:18 -0400

    switch to table layout

--- client/src/components/SketchCard.js
+++ client/src/components/SketchCard.js
@@ -5,19 +5,25 @@ export default class SketchCard extends Component {
   render(){
     const sketch = this.props.sketch
     return(
-      <div>
-        <SketchField
-          ref="sketch"
-          width='200px'
-          height='200px'
-          defaultData={sketch.data}
-          defaultDataType="json"
-          tool={Tools.Pan}
-        />
-        <div className="sketch-info">
-          <h3>{sketch.id}</h3>
-        </div>
-      </div>
+      <table className="w3-table sketch-card-container">
+        <tbody>
+        <tr>
+        <td>
+          <SketchField
+            ref="sketch"
+            width='200px'
+            height='200px'
+            defaultData={sketch.data}
+            defaultDataType="json"
+            tool={Tools.Pan}
+          />
+        </td>
+        <td className="sketch-info">
+          <h1>{sketch.id}</h1>
+        </td>
+        </tr>
+        </tbody>
+      </table>
     )
   }
 
