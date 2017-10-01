8bbdc90a0335ecbb4c7ce25e2e55001889810891
Luisa Scavo <luisascavo@gmail.com>
2017-10-01 11:54:56 -0400

    assign sketch prop to variable, fix indentation, add .sketch-info div

--- client/src/components/SketchCard.js
+++ client/src/components/SketchCard.js
@@ -3,16 +3,20 @@ import {SketchField, Tools} from 'react-sketch';
 
 export default class SketchCard extends Component {
   render(){
+    const sketch = this.props.sketch
     return(
       <div>
         <SketchField
-            ref="sketch"
-            width='200px'
-            height='200px'
-            defaultData={this.props.sketch.data}
-            defaultDataType="json"
-            tool={Tools.Pan}
-          />
+          ref="sketch"
+          width='200px'
+          height='200px'
+          defaultData={sketch.data}
+          defaultDataType="json"
+          tool={Tools.Pan}
+        />
+        <div className="sketch-info">
+          <h3>{sketch.id}</h3>
+        </div>
       </div>
     )
   }
