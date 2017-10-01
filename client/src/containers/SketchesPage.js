ccf4d6536df7f65d253fc2ff61c4622d30ea6fb9
Luisa Scavo <luisascavo@gmail.com>
2017-10-01 12:34:20 -0400

    add className .sketch-cards-container

--- client/src/containers/SketchesPage.js
+++ client/src/containers/SketchesPage.js
@@ -11,7 +11,7 @@ class SketchesPage extends Component {
   }
   render(){
     return(
-      <div>{
+      <div className="sketch-cards-container">{
         this.props.sketches.map((sketch, key) =>{ return(
           <SketchCard sketch={sketch} key={sketch.id}/>
         )})}
