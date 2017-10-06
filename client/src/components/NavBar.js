171b4455cae96c1d870871b7a794755887d0438d
Luisa Scavo <luisascavo@gmail.com>
2017-10-06 18:09:37 -0400

    add links to nav

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,8 +4,8 @@ export default class NavBar extends Component {
   render(){
     return(
       <div className='nav-bar'>
-        <div className='nav-item'>Draw</div>
-        <div className='nav-item'>Recent Drawings</div>
+        <div className='nav-item'><a href="/">Draw</a></div>
+        <div className='nav-item'><a href="/sketches">Recent</a></div>
       </div>
     )
   }
