21a60e34e00d24f7ee6ac6f5a9add6298008ac9f
Luisa Scavo <luisascavo@gmail.com>
2017-10-06 18:01:54 -0400

    Add wrapping div and menuName

--- client/src/components/MenuSelect.js
+++ client/src/components/MenuSelect.js
@@ -3,9 +3,12 @@ import React, {Component} from 'react';
 class MenuSelect extends Component {
   render(){
     return(
+      <div className="menu">
+      <h3>{this.props.menuName}</h3>
       <select onChange={this.props.onChange} defaultValue={this.props.value}>
         {this.props.children}
       </select>
+      </div>
     )
   }
 }
